4e2d700bbc5c585520a25ca4b4b72eb9f9c18a9d
Andrés I. <suurrend3r@gmail.com>
2025-02-14 02:05:09 -0300

    done

--- add_linenumber.c
+++ add_linenumber.c
@@ -13,6 +13,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Agregá enumeración de cada línea de un archivo.
 // Vim agrega un salto de línea siempre al final, 
@@ -20,18 +21,28 @@
 // echo -n -e "Hola\nEsto es\nuna prueba"
 int main(void){
     FILE *fp = fopen("/tmp/asdf", "rw+");
+    FILE *fq = fopen("/tmp/resultado", "w+");
     int lineas = 1;
 
     int c;
-    printf("%d: ",lineas);
+
+    char buffer[3] = "1: ";
+    for (int i = 0; i < strlen(buffer); i++){
+        putc(buffer[i], fq);
+    }
+
     while (( c = getc(fp) ) != EOF){
-        printf("%c",c);
+        putc(c, fq);
         if ( c == '\n'){ 
             lineas++;
-            printf("%d: ",lineas);
+            sprintf(buffer, "%d :", lineas); 
+            for (int i = 0; i < strlen(buffer); i++){
+                putc(buffer[i], fq);
+            }
         }
     }
     printf("\n");
+
     printf("Cantidad de lineas: %d\n",lineas);
 
     return 0;
