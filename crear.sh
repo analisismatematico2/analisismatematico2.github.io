ecd509cf33400a9594ef5b7298d3b2da079bb478
Andrés I. <suurrend3r@gmail.com>
2025-07-08 16:11:36 -0300

    asd

--- crear.sh
+++ crear.sh
@@ -5,9 +5,10 @@ if [[ -z "$1" ]]; then
 fi
 tree -H '' -d -o index.html # Solo carpetas
 
-find . -type d! -path.! -path "./.git*" | while read dir; do
-  (cd "$dir" && tree -H '' -d -o index.html && sed -i '1i <a href="../index.html">⬆️ Inicio</a><br>' index.html)
+find . -type d! -path '*/.*' | while read dir; do
+  (cd "$dir" && tree -H '' -o index.html && sed -i '1i <a href="../index.html">⬆️ Inicio</a><br>' index.html)
 done
+
 
 
 
