958a403be56da8ccd94eb59b28055bfef7a160a4
Andrés I. <suurrend3r@gmail.com>
2025-07-08 11:33:18 -0300

    capitalismo

--- crear.sh
+++ crear.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+if [[ -z "$1" ]]; then
+  echo "Uso: $0 commit_msg"
+  exit;
+fi
 tree -H '' -d -o index.html # Solo carpetas
 for dir in */; do
   (cd "$dir" && tree -H '' -o index.html)
 done
+git add . && git commit -m $1 && git push
 
