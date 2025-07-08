a88f1355b2408f95b38af8851e3647313ed655f9
Andrés I. <suurrend3r@gmail.com>
2025-07-08 15:46:25 -0300

    subfolder

--- crear.sh
+++ crear.sh
@@ -8,7 +8,7 @@ tree -H '' -d -o index.html # Solo carpetas
 #for dir in */; do
 #  (cd "$dir" && tree -H '' -o index.html && sed -i '1i <a href="../index.html">⬆️ Inicio</a><br>' index.html)
 #done
-find. -type d | while read dir; do
+find . -type d | while read dir; do
   (cd "$dir" && tree -H '' -d -o index.html && sed -i '1i <a href="../index.html">⬆️ Inicio</a><br>' index.html)
 done
 
