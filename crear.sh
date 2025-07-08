feedbe37b4cf058f8dcf7265db1ca54b28415e6a
Andrés I. <suurrend3r@gmail.com>
2025-07-08 15:46:10 -0300

    subfolder

--- crear.sh
+++ crear.sh
@@ -5,9 +5,13 @@ if [[ -z "$1" ]]; then
 fi
 tree -H '' -d -o index.html # Solo carpetas
 
-for dir in */; do
-  (cd "$dir" && tree -H '' -o index.html && sed -i '1i <a href="../index.html">⬆️ Inicio</a><br>' index.html)
+#for dir in */; do
+#  (cd "$dir" && tree -H '' -o index.html && sed -i '1i <a href="../index.html">⬆️ Inicio</a><br>' index.html)
+#done
+find. -type d | while read dir; do
+  (cd "$dir" && tree -H '' -d -o index.html && sed -i '1i <a href="../index.html">⬆️ Inicio</a><br>' index.html)
 done
+
 
 
 git add . && git commit -m $1 && git push
