50bbfc16b011bf6d86a4fe60c42818ba79aa09b6
Andrés I. <suurrend3r@gmail.com>
2025-07-08 12:48:04 -0300

    asdf

--- crear.sh
+++ crear.sh
@@ -4,8 +4,10 @@ if [[ -z "$1" ]]; then
   exit;
 fi
 tree -H '' -d -o index.html # Solo carpetas
-for dir in */; do
-  (cd "$dir" && tree -H '' -o index.html)
-done
-git add . && git commit -m $1 && git push
 
+for dir in */; do
+  (cd "$dir" && tree -H '' -o index.html && sed -i '1i <a href="../index.html">⬆️ Inicio</a><br>' index.html)
+done
+
+
+git add . && git commit -m $1 && git push
