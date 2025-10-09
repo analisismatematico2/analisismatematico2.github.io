a899aabaa86132c0c6782de7665136a483cd14b1
Andrés I. <suurrend3r@gmail.com>
2025-10-09 18:10:05 -0300

    rss

--- crear.sh
+++ crear.sh
@@ -12,5 +12,16 @@ find . -type d ! -path '*/.*' ! -path '.' | while read dir; do
   (cd "$dir" && tree -H '' -o index.html && sed -i '1i <a href="../index.html">⬆️ Subir</a><br>' index.html)
 done
 
+# Generar sitemap.xml simple
+{
+  echo '<?xml version="1.0" encoding="UTF-8"?>'
+  echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+  find . -name "*.html" ! -path "./.*" | sort | while read -r f; do
+    url="https://cursoelectricidad.github.io/${f#./}"
+    echo "  <url><loc>${url}</loc></url>"
+  done
+  echo '</urlset>'
+} > sitemap.xml
+
 
 git add . && git commit -m $1 && git push
