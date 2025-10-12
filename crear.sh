9538353868c4c858705307e98fa53b7f9f04d7aa
Andrés I. <suurrend3r@gmail.com>
2025-10-12 00:05:43 -0300

    centralizado

--- crear.sh
+++ crear.sh
@@ -9,7 +9,7 @@ tree -H '' -d -o index.html
 
 # Busca carpetas, ignora la carpeta actual, las carpetas ocultas 
 find . -type d ! -path '*/.*' ! -path '.' | while read dir; do 
-  (cd "$dir" && tree -H '' -o index.html && sed -i '1i <a href="../index.html">⬆️ Subir</a><br>' index.html)
+  (cd "$dir" && tree  -I '*.md' -H '' -o index.html && sed -i '1i <a href="../index.html">⬆️ Subir</a><br>' index.html)
 done
 
 BASE_URL="https://cursoelectricidad.github.io"
