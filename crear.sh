912a7659c0196ddebf9adf8700332e0201361745
Andrés I. <suurrend3r@gmail.com>
2025-11-21 21:19:24 -0300

    solucion

--- crear.sh
+++ crear.sh
@@ -5,11 +5,11 @@ if [[ -z "$1" ]]; then
 fi
 
 # Solo carpetas
-tree -H '' -d -o index.html && sed -i '1i <link rel="stylesheet" href="/xterm.css">' index.html
+tree -H '' -d -o index.html 
 
 # Busca carpetas, ignora la carpeta actual, las carpetas ocultas 
 find . -type d ! -path '*/.*' ! -path '.' | while read dir; do 
-  (cd "$dir" && tree  -I '*.md' -H '' -o index.html && sed -i '1i <a href="../index.html">⬆️ Subir</a><br><link rel="stylesheet" href="/xterm.css">' index.html)
+  (cd "$dir" && tree -r -I '*.md' -H '' -o index.html && sed -i '1i <a href="../index.html">⬆️ Subir</a><br><hr>' index.html)
 done
 
 BASE_URL="https://analisismatematico2.github.io"
